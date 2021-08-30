eed0be3fe7b8d35eaf4e46c69c0ea06255ec6ed6
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-30 18:26:29 +0100

    FilmList skeleton added

---
+++ src/components/filmlist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FilmItem from './filmitem';
+
+const FilmList = () => {
+
+
+
+    return (
+
+        <>
+        <h2>Film List</h2>
+        <FilmItem />
+        </>
+
+    )
+}
+
+export default FilmList;
