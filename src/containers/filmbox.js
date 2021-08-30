451ce89502df46a5f9b09671457dbd20f10e2563
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-30 18:20:05 +0100

    FilmBox page skeleton

---
+++ src/containers/filmbox.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import FilmList from '../components/filmlist';
+
+const FilmBox = () => {
+
+
+
+    return (
+
+        <>
+        <h1>Upcoming Releases</h1>
+        <FilmList/>
+        <hr />
+        <a href="https://www.imdb.com/calendar/?region=gb" target="_blank" title="View more upcoming releases on IMDB">View more upcoming releases</a>
+        </>
+    )
+
+}
+
+
+export default FilmBox;
